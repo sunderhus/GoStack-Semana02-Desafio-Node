de5618f7c89f8648bd4a838dabc3a5fa32ad7942
Matheus Sunderhus <matheus.sunderhus@interprocess.com.br>
2021-02-10 19:30:08 -0300

    featur: Add cors to project.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
+app.use(
+  cors({
+    origin: '*',
+  }),
+);
 app.use(express.json());
 app.use(routes);
 
